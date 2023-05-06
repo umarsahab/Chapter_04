765afcba473caf4865d04295716580b0f27e533f
umarsahab <121605324+umarsahab@users.noreply.github.com>
2023-05-06 14:31:04 +0500

    submit

---
+++ app.js
@@ -0,0 +1,24 @@
+// Question 1
+var myName, country, userName;
+
+// Question 2
+//legal names
+var car, carNum, num123, my_Name , _user;
+
+//illegal names
+// var 123num, my-car , slice, switch, false;
+
+// Question 3
+// a) A heading stating “Rules for naming JS variables”
+// b) Variable names can only contain ______, ______,
+// ______ and ______.
+// For example $my_1stVariable
+// c) Variables must begin with a ______, ______ or
+// _____. For example $name, _name or name
+// d) Variable names are case _________
+// e) Variable names should not be JS _______;
+
+document.write("Variable names can only contain , numbers, $ and _.For example : $my_1stVariable" + "<br />")
+document.write("Variables must begin with a , $ or _. For example $name, _name or name"  + "<br />")
+document.write("Variable names are case sensitive"  + "<br />")
+document.write("Variable names should not be JS keywords")
